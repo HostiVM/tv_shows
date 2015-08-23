70789e8e5c01ca0fb4e5f07339aa1e7447be3607
Hosti <kontakt@vit-meloun.net>
2015-08-23 19:38:00 +0200

    Add Arel gem and scopes on Event model

--- Gemfile
+++ Gemfile
@@ -51,3 +51,4 @@ group :development do
 end
 
 gem 'sofa'
+gem 'arel'
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,11 @@
 class Event < ActiveRecord::Base
   belongs_to :show
+
+  default_scope do
+    order :release
+  end
+
+  scope :upcoming, -> do
+    where arel_table[:release].gteq(DateTime.now)
+  end
 end
